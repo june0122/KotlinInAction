39ebb3848a00794f6e747fabec010321ef47ba47
june0122 <june0122@naver.com>
2022-02-15 20:32:18 +0900

    게터와 세터에서 뒷받침하는 필드에 접근

---
+++ src/classes/property/BackingFieldSample.kt
@@ -0,0 +1,16 @@
+package classes.property
+
+class User(val name: String) {
+    var address: String = "unspecified"
+        set(value) {
+            println("""
+                Address was changed for $name:
+                "$field" -> "$value".""".trimIndent()) // backing field 값 읽기
+            field = value // backing field 값 변경하기
+        }
+}
+
+fun main() {
+    val user = User("Alice")
+    user.address = "Elsenheimerstrasse 47, 80687 Muenchen"
+}
